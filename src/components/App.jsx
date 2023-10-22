7ac8106dcc3786b98332b6c6552d10f61a0a336f
MartaKoss27 <27691497m@gmail.com>
2023-10-22 16:14:10 +0200

    Part 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,74 @@
 import React, { Component } from 'react';
+import { Form } from './Form';
+import { nanoid } from 'nanoid';
 
 export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  //nanoid = nanoid();
+
+  //FORM
+  handleSubmit = event => {
+    event.preventDefault();
+    const form = event.currentTarget;
+
+    const {
+      name: { value: name },
+    } = form.elements;
+    const newContact = {
+      id: nanoid(),
+      name,
+    };
+
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, newContact],
+    }));
+  };
+  //CONTACTS
+  handleClick = id => {
+    const { contacts } = this.state;
+    const deleteContact = contacts.find(contact => contact.id === id);
+
+    if (deleteContact) {
+      this.setState(prevState => ({
+        contacts: prevState.contacts.filter(contact => contact.id !== id),
+      }));
+    }
+  };
+
   render() {
+    console.log(this);
     return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        Pierdole to wszystko
+      <div>
+        <div>
+          <h1>Phonebook</h1>
+          <Form onSubmit={this.handleSubmit} />
+          <h2>Contacts list</h2>
+          <Contacts
+            contacts={this.state.contacts}
+            removeContact={this.handleClick}
+          />
+        </div>
       </div>
     );
   }
 }
+//CONTACTS
+
+const Contacts = ({ contacts, removeContact }) => {
+  return (
+    <ul>
+      {contacts.map(contact => (
+        <li key={contact.id}>
+          {contact.name}
+          <button type="button" onClick={removeContact}>
+            REMOVE
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,1 +1,20 @@
+import React from 'react';
 
+export const Form = ({ onSubmit }) => {
+  return (
+    <form onSubmit={onSubmit}>
+      <label>
+        Name
+        <input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan."
+          required
+          placeholder="Enter name"
+        />
+      </label>
+      <button type="submit">Add contact</button>
+    </form>
+  );
+};
