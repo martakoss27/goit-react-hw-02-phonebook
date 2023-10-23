1e7f49342300cadb6dea13e34bb82d647cd8861b
MartaKoss27 <27691497m@gmail.com>
2023-10-23 14:09:43 +0200

    final update
    no double contacts functionality added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,12 +8,7 @@ import { styled } from 'styled-components';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
     name: '',
     number: '',
@@ -31,17 +26,25 @@ export class App extends Component {
       number: { value: number },
     } = form.elements;
 
-    const newContact = {
-      id: nanoid(),
-      name,
-      number,
-    };
+    const contactExists = this.state.contacts.find(
+      contact => contact.name.toLowerCase() === name.toLowerCase()
+    );
 
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, newContact],
-    }));
-    Notiflix.Notify.success('New contact succesfully added!');
-    form.reset();
+    if (contactExists) {
+      Notiflix.Notify.warning(`${name} is already in contacts.`);
+    } else {
+      const newContact = {
+        id: nanoid(),
+        name,
+        number,
+      };
+
+      this.setState(prevState => ({
+        contacts: [...prevState.contacts, newContact],
+      }));
+      Notiflix.Notify.success('New contact succesfully added!');
+      form.reset();
+    }
   };
 
   //CONTACTS
@@ -63,7 +66,6 @@ export class App extends Component {
   };
 
   render() {
-    console.log(this);
     const { contacts, filter } = this.state;
     const filterSearch = contacts.filter(contact =>
       contact.name.toLowerCase().includes(this.state.filter)
